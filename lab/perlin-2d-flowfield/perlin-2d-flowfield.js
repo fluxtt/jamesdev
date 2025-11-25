20095a6336bcaf58ef75b7896be115bd0edf9565
fluxtt <james.tibbott@hotmail.co.uk>
2025-11-25 21:56:47 +0000

    Initial setting up the page and index

---
+++ lab/perlin-2d-flowfield/perlin-2d-flowfield.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+}
+
+// Resize canvas if you stretch the window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
